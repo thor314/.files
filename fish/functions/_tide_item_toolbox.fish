6470696cc40718cbd1b0daa31552214404477c41
thor <thorck@protonmail.com>
2022-11-30 13:20:09 -0800

    pop-os-2022-11-30 21:20UTC

--- fish/functions/_tide_item_toolbox.fish
+++ fish/functions/_tide_item_toolbox.fish
@@ -1,4 +1,3 @@
 function _tide_item_toolbox
-    test -e /run/.toolboxenv &&
-        _tide_print_item toolbox $tide_toolbox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
+    test -e /run/.toolboxenv && _tide_print_item toolbox $tide_toolbox_icon' ' $hostname
 end
