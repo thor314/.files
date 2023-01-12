23bc158f34b76253b15b1a8bc0dc8b9c23dcf342
Thor Kamphefner <thorck@pm.me>
2023-01-11 22:00:06 -0800

    pop-os-2023-01-12 06:00UTC

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,23 +7,25 @@ require "core.options"
 
 -- setup packer + plugins
 local fn = vim.fn
+local opt = vim.opt
+local api = vim.api
 local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+  api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
   print "Cloning packer .."
   fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
 
   -- install plugins + compile their configs
-  vim.opt.cmd "packadd packer.nvim"
+  opt.cmd "packadd packer.nvim"
   require "plugins"
-  vim.opt.cmd "PackerSync"
+  opt.cmd "PackerSync"
 
   -- install binaries from mason.nvim & tsparsers
-  vim.api.nvim_create_autocmd("User", {
+  api.nvim_create_autocmd("User", {
     pattern = "PackerComplete",
     callback = function()
-      vim.opt.cmd "bw | silent! MasonInstallAll" -- close packer window
+      opt.cmd "bw | silent! MasonInstallAll" -- close packer window
       require("packer").loader "nvim-treesitter"
     end,
   })
