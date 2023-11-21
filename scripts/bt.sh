bf8e35c855e21ac86471ad8b10ae9d217642270d
Thor Kamphefner <thorck@pm.me>
2023-11-21 11:10:03 -0500

    pop-os-2023-11-21 16:10UTC

--- scripts/bt.sh
+++ scripts/bt.sh
@@ -6,10 +6,12 @@ current_status=$(bluetoothctl show | grep "Powered" | awk '{ print $2 }')
 if [ "$current_status" = "yes" ]; then
     # Bluetooth is on, turn it off
     echo "Turning Bluetooth off..."
+    notify-send -t 1000 "Turning Bluetooth off..."
     bluetoothctl power off
 elif [ "$current_status" = "no" ]; then
     # Bluetooth is off, turn it on
     echo "Turning Bluetooth on..."
+    notify-send -t 1000 "Turning Bluetooth on..."
     bluetoothctl power on
 else
     echo "Error: Could not determine Bluetooth power status."
