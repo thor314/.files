f46ad3bac04572b992770f86e3954e19debaeccb
Thor Kamphefner <thorck@pm.me>
2024-01-11 11:03:44 -0800

    profile and fish config clean

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/fish
-# Thor's fishy config
 if status is-interactive
     # Commands to run in interactive sessions can go here
     # set shell to fish, don't set everywhere, we been warned that fish not POSIX so mebe breaks elsewhere
     set -gx SHELL 'usr/bin/fish'
+
     # Overwrite default greeting
     function fish_greeting 
         echo "hello Thor" 
@@ -12,32 +12,41 @@ if status is-interactive
     # only need these for interactive sessions
     source $HOME/.files/fish/abbr.fish
     source $HOME/.files/fish/bind.fish
+
+    if test (hostname) = "starchy" 
+      # default is TERM=alacritty; this fixes weird input bugs with alacritty
+      set TERM xterm-256color 
+    end
 end
 
-# load muh secret environment variables
+# load secret environment variables
 gpg -qd "$HOME/.private/secrets.gpg" | source
 
-# store selected editor or it dirties home env
-set -gx SELECTED_EDITOR /usr/bin/vim.gtk3
-
-# use bat to display man pages with syntax highlighting etc
+ # # # ## ##### ## # # #
+# ENVIRONMENT VARIABLES #
+ # # # ## ##### ## # # #
+# keep these in config.fish for machine setup convenience) 
+# use bat to display man pages with syntax highlighting etc 
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
-# pnpm is our fast friend https://pnpm.io/
-set -gx PNPM_HOME "/home/thor/.local/share/pnpm"
+# Uncomment the following line to run zellij at start (assuming bash is your shell, which this is not)
+# set -gx ZELLIJ_AUTO_ATTACH=true # attach to any pre-existing sessions
+# eval "$(zellij setup --generate-auto-start bash)"
 
-# bug: doesn't appear to change the output of `jupyter --config-dir`
-# https://docs.jupyter.org/en/latest/use/jupyter-directories.html
-set -g JUPYTER_CONFIG_DIR "$HOME/.files/jupyter"
-
- ##########################################################################
-# PATH UPDATES SHOULD BE IN .profile THOR, WHAT ARE YOU DOING TO YOURSELF? #
- ########################################################################### 
-# moved to profile
 # #########
  # ARCHIVE #
 # #########
+ # X # X X # X X ## X X # X X # X X # X
+# PATH - SHOULD BE IN .PROFILE INSTEAD # - deprecate on 2024-10-11
+ # X # X X # X X ## X X # X X # X X # X
+# store selected editor or it dirties home env
+# 2024-01-11 deprecation planned
+# set -gx SELECTED_EDITOR /usr/bin/vim.gtk3
 
+# 2024-01-11 deprecation planned
+# bug: doesn't appear to change the output of `jupyter --config-dir`
+# https://docs.jupyter.org/en/latest/use/jupyter-directories.html
+# set -gx JUPYTER_CONFIG_DIR "$HOME/.files/jupyter"
 # 2023-12-30 - experiment; silence warning "nvm: can't use node latest" warnings
 # nvm use latest >> /dev/null # puts npm in path, and b quiet
 
--- profile
+++ profile
@@ -1,35 +1,38 @@
 # TK: this file is run on login, but not for subsequent terminal creation.
-#
-# Generally used to set up env variables at login,
+# Generally used to set up global env variables at login, FOR ALL LOGIN SHELLS.
 # eg: https://www.unix.com/unix-for-advanced-and-expert-users/5477-looking-profile-examples.html
-#
-# ~/.profile: executed by the command interpreter for login shells.
-# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
-# exists.
-# the default umask is set in /etc/profile; for setting the umask
-# for ssh logins, install and configure the libpam-umask package.
-#umask 022
-export PROFILE_TEST=1 
-export ZDOTDIR="$HOME/.zsh.d" # redundant
+# This file is not read by bash, if ~/.bash_profile or ~/.bash_login exists.
+export PROFILE_TEST="2024-01-11"
 
-## PATH variables
+ # X # X # X # X # X 
+# X # # PATH  # # X #
+ # X # X # X # X # X
 export GOPATH="$HOME/go"
 export GOROOT=/usr/local/go
-# fpath+="$HOME/.zsh.d/zsh_functions" # alacritty completions
-PATH="$PATH:$GOPATH/bin:$GOROOT/bin"
-PATH="$PATH:$HOME/.cargo/bin/bat-extras/bin"
-PATH="$PATH:$HOME/.cargo/bin"
-PATH="$PATH:$HOME/.local/bin"
-PATH="$PATH:$HOME/.share/bin"
-PATH="$HOME/.elan/bin:$PATH"
-# PATH=":$HOME/.files/local-bin/npm-bin"
+PATH="$PATH:$HOME/.local/bin:$HOME/.share/bin" # misc+python binaries
+PATH="$PATH:$GOPATH/bin:$GOROOT/bin" # golang
+PATH="$PATH:$HOME/.cargo/bin:$HOME/.cargo/bin/bat-extras/bin" # rust
+PATH="$PATH:$HOME/.elan/bin" # lean-lang
+PATH="$PATH:$PNPM_HOME" # pnpm - fast npm substitute
+export PATH
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export PYTHONPATH="$PYTHONPATH:/usr/lib/python3/dist-packages/sage"
 
-# configure shell with cargo commands 
+# other environment var settings to set across *all* login shells
+export SELECTED_EDITOR="usr/bin/vim.gtk3"
+
+# #########
+ # ARCHIVE #
+# #########
+# keyboard set - 2024-10-11 - stop deduplication, deprecation pending
+# setxkbmap dvorak -option ctrl:nocaps
+# configure shell with cargo stuff - 2024-01-11 - is this only necessary for install? haven't needed since
 # source "/home/thor/.cargo/env" 
 
-# Please ser, may I have my keyboard
-setxkbmap dvorak -option ctrl:nocaps
+## Zsh - deprecated 2022
+# fpath+="$HOME/.zsh.d/zsh_functions" # alacritty completions
+# export ZDOTDIR="$HOME/.zsh.d" 
 
-
-# declutter home env
-export SELECTED_EDITOR="usr/bin/vim.gtk3"
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+# umask 022
