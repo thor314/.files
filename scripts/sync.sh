f1488207e1a690cc0c49c12ed249a304b0341274
Thor Kamphefner <thorck@pm.me>
2024-01-12 18:57:37 -0800

    sync fix

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/fish
 # argument to run on my other machines to keep them in sync
 
-if type -q taplo
+if not type -q taplo
   wget https://github.com/tamasfe/taplo/releases/latest/download/taplo-linux-x86.gz
   gunzip taplo-linux-x86.gz
   chmod +x taplo-linux-x86
   mv taplo-linux-x86 ~/.cargo/bin/taplo
+end
+
+if not type -q gi
+  fisher install oh-my-fish/plugin-gi
+  gi update-completions
 end
 
