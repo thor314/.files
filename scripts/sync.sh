38ca6dade5d41eeb39871c61e834babe5925289d
Thor Kamphefner <thorck@pm.me>
2024-01-12 19:00:15 -0800

    "boorb-2024-01-13 03:00UTC"

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,6 +2,7 @@
 # argument to run on my other machines to keep them in sync
 
 if not type -q taplo
+  # https://taplo.tamasfe.dev/cli/introduction.html
   wget https://github.com/tamasfe/taplo/releases/latest/download/taplo-linux-x86.gz
   gunzip taplo-linux-x86.gz
   chmod +x taplo-linux-x86
@@ -9,6 +10,7 @@ if not type -q taplo
 end
 
 if not type -q gi
+  # https://github.com/oh-my-fish/plugin-gi
   fisher install oh-my-fish/plugin-gi
   gi update-completions
 end
