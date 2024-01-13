d53f24e1c7553c3bafd80a6381c0d1c1940783dd
Thor Kamphefner <thorck@pm.me>
2024-01-12 18:27:10 -0800

    taplo

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/fish
 # argument to run on my other machines to keep them in sync
 
+if type -q taplo
+  wget https://github.com/tamasfe/taplo/releases/latest/download/taplo-linux-x86.gz
+  gunzip taplo-linux-x86.gz
+  chmod +x taplo-linux-x86
+  mv taplo-linux-x86 ~/.cargo/bin/taplo
+end
+
