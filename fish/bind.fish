c103f14d928eeaa70f0f48d35e0f06341c9e565b
Thor Kamphefner <thorck@pm.me>
2024-01-25 15:50:32 -0800

    "crow-2024-01-25-23:50UTC"

--- fish/bind.fish
+++ fish/bind.fish
@@ -1,11 +1,14 @@
 # My bindings. 
 # Docs: https://fishshell.com/docs/current/cmds/bind.html 
 bind \cZ 'fg'
-bind \cW 'exit'
-bind \cS 'zellij'
+# bind \cW 'exit' # don't need, but keep as proof of concept
+bind \cS 'zellij' # bug? immediately quits
 
-# forward/back
+# very nice forward/back
 bind \eo 'prevd; commandline -f repaint'
-bind \el 'lsd --icon=never; commandline -f repaint'
-bind \eL 'lsd -a --icon=never; commandline -f repaint'
+bind \e\, 'prevd; commandline -f repaint'
 bind \ei 'nextd; commandline -f repaint'
+bind \e\. 'nextd; commandline -f repaint'
+
+bind \el 'lsd --icon=never; commandline -f repaint' 
+bind \eL 'lsd -a --icon=never; commandline -f repaint'
