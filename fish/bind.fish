3e1a5e2a4ea53dd1fe960499fb2b9f36a995ca6c
Thor Kamphefner <thorck@pm.me>
2024-01-25 15:55:53 -0800

    crow-2024-01-25-23:55UTC

--- fish/bind.fish
+++ fish/bind.fish
@@ -4,11 +4,12 @@ bind \cZ 'fg'
 # bind \cW 'exit' # don't need, but keep as proof of concept
 bind \cS 'zellij' # bug? immediately quits
 
-# very nice forward/back
-bind \eo 'prevd; commandline -f repaint'
+# very nice forward/back binds
 bind \e\, 'prevd; commandline -f repaint'
-bind \ei 'nextd; commandline -f repaint'
 bind \e\. 'nextd; commandline -f repaint'
 
+set COMMIT_MSG "$(hostname)-$(date -u +%Y-%m-%d-%H:%M%Z)"
+bind \eg (tk-git-add-all-commit-message-push-create-upstream-branch $COMMIT_MSG) ; commandline -f repaint
+
 bind \el 'lsd --icon=never; commandline -f repaint' 
-bind \eL 'lsd -a --icon=never; commandline -f repaint'
+bind \eL 'lsd -al --icon=never; commandline -f repaint'
