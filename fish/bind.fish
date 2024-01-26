3aaab1284aae03bef01f3407129231851c6fe2a5
Thor Kamphefner <thorck@pm.me>
2024-01-26 10:20:09 -0800

    "boorb-2024-01-26-18:20UTC"

--- fish/bind.fish
+++ fish/bind.fish
@@ -7,21 +7,20 @@ bind \cZ fg
 # very nice forward/back binds
 bind \e\, "prevd; commandline -f repaint" 
 bind \e\. "nextd; commandline -f repaint"
-bind \eo "prevd; commandline -f repaint"
-bind \ei "nextd; commandline -f repaint"
+# bind \eo "prevd; commandline -f repaint"
+# bind \ei "nextd; commandline -f repaint"
 
 # set COMMIT_MSG "$(hostname)-$(date -u +%Y-%m-%d-%H:%M%Z)"
-bind \ea "nohup alacritty --working-directory (pwd) >/dev/null 2>&1 & disown 2>/dev/null" # new alacritty in same dir
 bind \ec "code ."
-bind \ed "vi $HOME/.files/install.conf.yaml"
-bind \ef "vi $HOME/.files/fish/config.fish"
-bind \el "lsd --icon=never; commandline -f repaint"
-bind \ev "vi $HOME/.vimrc"
-
-bind \eC "cargo check -q  ; commandline -f repaint"
-bind \eL "lsd -al --icon=never; commandline -f repaint"
-bind \eF "cargo fmt -v; commandline -f repaint"
-bind \eT "cargo test"
-bind \eR "cargo run"
 bind \er "rusty-rain -c numbers"
+
+bind \eA "nohup alacritty --working-directory (pwd) >/dev/null 2>&1 & disown 2>/dev/null" # new alacritty in same dir
+bind \eC "cargo check -q  ; commandline -f repaint"
+bind \eD "vi $HOME/.files/install.conf.yaml"
+bind \eL "lsd --icon=never; commandline -f repaint"
+bind \eF "cargo fmt -v; commandline -f repaint"
+bind \eR "cargo run"
+bind \eS "vi $HOME/.files/fish/config.fish"
+bind \eT "cargo test"
+bind \eV "vi $HOME/.vimrc"
 
