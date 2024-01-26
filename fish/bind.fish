49ab8ccc504d5bd05b9e7305001e0506e3714a1f
Thor Kamphefner <thorck@pm.me>
2024-01-25 17:11:03 -0800

    "crow-2024-01-26-01:10UTC"

--- fish/bind.fish
+++ fish/bind.fish
@@ -2,7 +2,6 @@
 # Docs: https://fishshell.com/docs/current/cmds/bind.html 
 # generally surround in \" for binds to work as expected
 bind \cZ fg
-bind \eS "zellij" #
 # bind \cW 'exit' # don't need, but keep as proof of concept
 
 # very nice forward/back binds
@@ -16,14 +15,12 @@ bind \ea "nohup alacritty --working-directory (pwd) >/dev/null 2>&1 & disown 2>/
 bind \ec "code ."
 bind \ed "vi $HOME/.files/install.conf.yaml"
 bind \ef "vi $HOME/.files/fish/config.fish"
-# bind \el lsd --icon=never; commandline -f repaint
-bind \ep "git push" 
-bind \eu "git pull"
+bind \el "lsd --icon=never; commandline -f repaint"
 bind \ev "vi $HOME/.vimrc"
 
 bind \eC "cargo check -q  ; commandline -f repaint"
 bind \eL "lsd -al --icon=never; commandline -f repaint"
-bind \eF "cargo fmt"
+bind \eF "cargo fmt -v; commandline -f repaint"
 bind \eT "cargo test"
 bind \eR "cargo run"
 
