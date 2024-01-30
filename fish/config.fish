6cf54daca11d0bc8ecd10a832962ca830245cd6f
Thor Kampefner <thorck@pm.me>
2024-01-30 10:36:53 -0800

    yet more ssh flunkery

--- fish/config.fish
+++ fish/config.fish
@@ -24,11 +24,10 @@ if status is-interactive
 
   # having ssh-issues? configure keychain
   # keychain --eval --quiet -Q | source # ensure agent is running
-  keychain --nogui ~/.ssh/id_ed25519 &>> /dev/null # if no key is not yet known, add key
-  
+  # keychain --nogui ~/.ssh/id_ed25519 &>> /dev/null # if no key is not yet known, add key
   # 2024-10-29 run these to ensure my ssh keys are loaded in a way that my cronjobs can access
+  # equivalent: `eval (ssh-agent -c)`, but looks for existing agents, avoids dupl/pass reentry 
   eval (keychain --eval -Q) &>> /dev/null # -Q is "quick" not quiet
-  # make sure the cron key is added 
   # keychain --nogui ~/.ssh/id_ed25519 -Q &>> /dev/null
 
   # even if set elsewhere, to avoid capslock vscode bug, keep this line
