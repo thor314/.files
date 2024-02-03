f0aefe5238562b4459400c35dfe0a08cbc0e8f42
Thor Kampefner <thorck@pm.me>
2024-02-03 11:45:09 -0800

    "boorb-2024-02-03-19:45UTC"

--- fish/rust_helpers.fish
+++ fish/rust_helpers.fish
@@ -36,8 +36,8 @@ abbr -a -g cyf  'cargo clippy --fix' # try to fix lints
 abbr -a -g cgtb 'tk-cargo-generate --bin cgtb'
 abbr -a -g cgtl 'tk-cargo-generate --lib cgtl'
 abbr -a -g ru rustup
-abbr -a -g rpnb tk-rust-playground-new --bin
-abbr -a -g rpnl tk-rust-playground-new --lib
+abbr -a -g rpnb 'tk-rust-playground-new --bin'
+abbr -a -g rpnl 'tk-rust-playground-new --lib'
 
 # cargo generate
 function tk-cargo-generate
