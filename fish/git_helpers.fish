a3e0bbac415ff82da9867ef159c706979395341c
Thor Kampefner <thorck@pm.me>
2024-02-03 18:40:03 -0800

    "boorb-2024-02-04-02:40UTC"

--- fish/git_helpers.fish
+++ fish/git_helpers.fish
@@ -27,20 +27,34 @@ abbr -a -g hpr "hub pull-request"
 abbr -a -g hi "hub issue"
 
 function tk-git-clone-and-cd # echo "clone from github convenience"
+  argparse --min-args=1 -- $argv
   hub clone $argv[1] --recursive
-  set repo_name (tk-path-to-name $argv[1]) # path-to-name is my helper function 
+  set repo_name (tk-path-to-name $argv[1])
   cd $repo_name
 end
 
-function tk-git-submodule-add # reminder to not use http, all sorts of weird cloning and pushing issues.
+function tk-git-submodule-add -d "add submodule to gitmodules" 
+  # reminder to not use http, all sorts of weird cloning and pushing issues.
+  argparse --min-args=1 -- $argv
   set repo_name (tk-path-to-name $argv[1])
   git submodule add git@github.com:thor314/$repo_name.git $argv[1]
   git add --all . && git commit -m "added submodule $repo_name"
 end
 
+# 2024-02-03 - untested
+function tk-git-submodule-foreach -d "better git submodule foreach"
+  argparse --min-args=1 -- $argv
+  set dir $argv[1]
+  set commands $argv[2..]
+  pushd dir
+  set -l modules (git config --file .gitmodules --get-regexp path | awk '{ print $2 }')
+  for m in $modules ; pushd m && (commands) && popd ; end
+  popd 
+end
 # a git helper that removes a submodule
 
 function tk-git-submodule-replace # for when accidentally committed a submodule instead of adding it
+  argparse --min-args=1 -- $argv
   echo "warn: maybe buggy"
   set repo_name (tk-path-to-name $argv[1])
   git rm -r --cached $repo_name
@@ -56,16 +70,20 @@ function tk-git-submodule-update-commits-recursive
 end
 
 function tk-git-add-all-commit-message-push # git commit and push convenience
+  argparse --min-args=1 -- $argv
   git add --all . --verbose
   git commit -m $argv[1]
   git push
 end
 function tk-git-add-all-commit-message-push-create-upstream-branch # new branch
+  argparse --min-args=1 -- $argv
   git add --all . --verbose
   git commit -m $argv[1]
   git push --set-upstream origin (git branch --show-current)
 end
 function tk-git-add-all-commit-message-push-create-repo # new repo
-  hub create $argv[2] # optional arg to allow private; i.e. -p, pass to hub create
+  argparse -p/private -- $argv
+  argparse --min-args=1 -- $argv
+  hub create $_flag_p # optional arg to allow private; i.e. -p, pass to hub create
   tk-git-add-all-commit-message-push-create-upstream-branch $argv[1]
 end
