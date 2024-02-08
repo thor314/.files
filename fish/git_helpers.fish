eb49094e1668e8ab27a4379dc9a09fd1457b4eca
Thor Kampefner <thorck@pm.me>
2024-02-07 18:10:04 -0800

    "boorb-2024-02-08-02:10UTC"

--- fish/git_helpers.fish
+++ fish/git_helpers.fish
@@ -56,10 +56,13 @@ end
 # We hate git submodules, but we also hate git-subtree
 # 2024-02-07 untested
 function tk-git-submodule-init -d "init submodules recursively, pull if exists, clone otherwise"
-  if not test -f .gitmodules; echo ".gitmodules file not found." && return 1; end
+  if not test -f .gitmodules ; echo "ERROR: .gitmodules file not found in (pwd)" && return 1 ;
+  else if not test -e .git ; echo "ERROR: .git not found in (pwd)" && return 1 ; end
 
+  # HACK: iterate over gitmodules, obtaining url and path name
   rg --only-matching -- 'path = (.+)' .gitmodules | string replace 'path = ' '' | while read -l path
-    set url (rg --only-matching -- "url = .+" .gitmodules | string match --regex -- "url = .+" | string replace "url = " '' | read -l)
+    set -l name $(string split "/" $path)[-1]
+    set -l url (rg --only-matching -- "url = (.+)$name.*.git" .gitmodules | string replace 'url = ' '')
 
     if not test -d $path
       echo "Directory does not exist. Cloning submodule $path..."
@@ -87,17 +90,18 @@ function tk-git-submodule-init -d "init submodules recursively, pull if exists, 
   end
 end
 
+# 02-07 untested
 function tk-git-submodule-update -d "Update submodules recursively, add untracked repos to .gitmodules, and pull updates"
-  if not test -f .gitmodules
-    echo ".gitmodules file not found." && return 1
-  end
+  if not test -f .gitmodules ; echo "ERROR: .gitmodules file not found in (pwd)" && return 1 ;
+  else if not test -e .git ; echo "ERROR: .git not found in (pwd)" && return 1 ; end
 
   # Find and process all directories containing a .git directory
   for dir in (fdfind -H  ".git\$" --prune)[2..] # skip current dir. Footgun: only run this from a .git root.
     set repo_path (dirname $dir) 
     
     if not rg -q "path = $repo_path" .gitmodules
-      echo "$repo_path is not known by .gitmodules. Adding it..."
+      echo "INFO: $repo_path is not known by .gitmodules. Adding it..."
+
       # Ensure repo is not mistakenly cached
       git rm --cached $repo_path >/dev/null 2>&1 || true
       set url (git -C $repo_path config --get remote.origin.url)
@@ -124,6 +128,44 @@ function tk-git-submodule-update -d "Update submodules recursively, add untracke
     end
   end
 end
+
+
+function tk-git-submodule-clean -d "Update submodules recursively, add untracked repos to .gitmodules, and pull updates"
+  if not test -f .gitmodules ; echo "ERROR: .gitmodules file not found in (pwd)" && return 1 ;
+  else if not test -e .git ; echo "ERROR: .git not found in (pwd)" && return 1 ; end
+
+  # Find and process all directories containing a .git directory
+  for dir in (fdfind -H  ".git\$" --prune)[2..] # skip current dir. Footgun: only run this from a .git root.
+    set repo_path (dirname $dir) 
+    
+    if not rg -q "path = $repo_path" .gitmodules
+      echo "INFO: $repo_path is not known by .gitmodules. Adding it..."
+
+      # Ensure repo is not mistakenly cached
+      git rm --cached $repo_path >/dev/null 2>&1 || true
+      set url (git -C $repo_path config --get remote.origin.url)
+      if test -n "$url"
+        git submodule add $url $repo_path
+        git add .gitmodules
+        git commit -m "Added submodule $repo_path"
+      else
+        echo "WARNING: Unable to find remote URL for $repo_path"
+        continue
+      end
+    end
+  end
+
+  # Pull updates for all known submodules
+  for submodule in (rg '\[submodule' .gitmodules -A2)
+    set path (echo $submodule | rg 'path')[3]
+    if test -d $path
+      echo "Pulling updates for submodule $path..."
+      if not git -C $path pull
+        echo "WARNING: Failed to update $path"
+        continue
+      end
+    end
+  end
 end
 
 function tk-git-submodule-add -d "add submodule to gitmodules" 
