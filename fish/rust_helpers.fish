6d6f0dc4b2b873f96593be77102ed618029af5e3
Thor Kampefner <thorck@pm.me>
2024-03-01 15:10:03 -0800

    "crow-2024-03-01-23:10UTC"

--- fish/rust_helpers.fish
+++ fish/rust_helpers.fish
@@ -26,13 +26,13 @@ abbr -a -g ctl 'cargo test -- --nocapture &> log'
 abbr -a -g ctlh 'cargo test -- --nocapture &> log; head log'
 abbr -a -g cy  'cargo clippy'
 
-abbr -a -g dm 'diesel migration'
-abbr -a -g dmr 'diesel migration run'
-abbr -a -g dmrd 'diesel migration redo'
-abbr -a -g dmrda 'diesel migration redo -a'
-abbr -a -g dmrv 'diesel migration revert'
-abbr -a -g dmrva 'diesel migration revert -a'
-abbr -a -g dmg 'diesel migration generate'
+# abbr -a -g dm 'diesel migration'
+# abbr -a -g dmr 'diesel migration run'
+# abbr -a -g dmrd 'diesel migration redo'
+# abbr -a -g dmrda 'diesel migration redo -a'
+# abbr -a -g dmrv 'diesel migration revert'
+# abbr -a -g dmrva 'diesel migration revert -a'
+# abbr -a -g dmg 'diesel migration generate'
 
 # other cargo commands:
 abbr -a -g cfc  "cargo fmt --all -- --check"
